0b1ff7c4c0226be5d5058588f0ac35669b4a5584
Juan Guillherme <codeinnove@gmail.com>
2024-11-18 19:24:37 -0300

    create types company

--- src/companies/types/Company.ts
+++ src/companies/types/Company.ts
@@ -4,7 +4,14 @@ export type Company = {
     email: string;
     //address: Address;
     tinNumber: string; //Esta propriedade armazena o número de identificação fiscal da empresa
-    //contacts: CompanyContact[]; //Esta propriedade armazena os contatos da empresa
+    contacts: CompanyContact[]; //Esta propriedade armazena os contatos da empresa
     createdAt: Date; //Esta propriedade armazena a data de criação do registro da empresa.
     updatedAt: Date; // Esta propriedade armazena a data da última atualização do registro da empresa
+  };
+
+export type CompanyContact = {
+    name: string;
+    email: string;
+    phone: string;
+    position: string;
   };
