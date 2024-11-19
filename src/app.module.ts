f15dbfec86ab68e55afe5b6bfd1097a339835676
Juan Guillherme <codeinnove@gmail.com>
2024-11-18 22:23:41 -0300

    add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CompaniesModule } from './companies/companies.module';
 import { EnterpriseModule } from './enterprise/enterprise.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ProposalsModule } from './proposals/proposals.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { PaymentsModule } from './payments/payments.module';
+import { CategorysModule } from './categorys/categorys.module';
+import { SkillsModule } from './skills/skills.module';
 
 @Module({
-  imports: [UsersModule, CompaniesModule, EnterpriseModule],
+  imports: [UsersModule, CompaniesModule, EnterpriseModule, ProjectsModule, ProposalsModule, ReviewsModule, PaymentsModule, CategorysModule, SkillsModule],
   controllers: [AppController],
   providers: [AppService],
 })
