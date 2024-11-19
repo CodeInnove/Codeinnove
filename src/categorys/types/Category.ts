8fb2d8c7d60bf785f3f7d28a628313d53889e5e7
Juan Guillherme <codeinnove@gmail.com>
2024-11-18 22:29:58 -0300

    add types

---
+++ src/categorys/types/Category.ts
@@ -0,0 +1,14 @@
+export type Categorys = {
+    _id: string;
+    name: string;
+    email: string;
+    phone: string;
+    birthdate: Date;
+    cpf: string;
+    rg: string;
+    password: string;
+    passwordResetToken: string;
+    passwordResetExpires?: Date;
+    //address: Address;
+
+}
---
+++ src/projects/types/Project.ts
@@ -0,0 +1,14 @@
+export type Projects = {
+    _id: string;
+    name: string;
+    email: string;
+    phone: string;
+    birthdate: Date;
+    cpf: string;
+    rg: string;
+    password: string;
+    passwordResetToken: string;
+    passwordResetExpires?: Date;
+    //address: Address;
+
+}
