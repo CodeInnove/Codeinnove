ad9cc4265cc3455a7ad17b759d8c9ed5cca0bd6b
Juan Guillherme <codeinnove@gmail.com>
2024-11-21 14:09:56 -0300

    feat(prisma) implements

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { CompaniesModule } from './companies/companies.module';
-import { EnterpriseModule } from './enterprise/enterprise.module';
-import { ProjectsModule } from './projects/projects.module';
-import { ProposalsModule } from './proposals/proposals.module';
-import { ReviewsModule } from './reviews/reviews.module';
-import { PaymentsModule } from './payments/payments.module';
-import { CategorysModule } from './categorys/categorys.module';
-import { SkillsModule } from './skills/skills.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+
+/*await this.$connect(): Aqui, o serviço se conecta ao banco de dados
+usando o Prisma. Isso garante que a conexão com o banco de dados 
+esteja estabelecida antes que a aplicação comece a processar 
+requisições. */
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
