075ec87d84bf30201f407be330d5caa2a05abe71
Juan Guillherme <codeinnove@gmail.com>
2024-11-22 10:10:07 -0300

    add crud users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UsersModule,  
-    PrismaModule],
+    PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
