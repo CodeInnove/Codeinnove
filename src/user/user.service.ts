73392a6d28b0c2998f65052ee0b830d2bd032cc5
Juan Guillherme <codeinnove@gmail.com>
2024-11-25 17:04:20 -0300

    add modules

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,33 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const data = {
-      ...CreateUserDto,
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
     };
 
-    const createdUser = await this.prisma.user.create({
-      data: undefined
-    });
+    const createdUser = await this.prisma.user.create({ data });
 
     return {
       ...createdUser,
       password: undefined,
-    }
+    };
   }
 
   findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
   }
-
-  
 }
