3623f29f1d592bfd90fcb0e4ef3f33b4a5dbcc36
Juan Guillherme <guilhermejuan729@gmail.com>
2024-11-27 16:56:50 -0300

    feat(auth): implement login generate jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,33 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { error } from 'console';
-import { UnsubscriptionError } from 'rxjs';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
+    
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    login(user: User): UserToken {
+        // trasfroma o user em um JWT
+        const payload: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            name: user.name
+         };
+
+        const jwtToken = this.jwtService.sign(payload)
+
+        return{
+            access_token: jwtToken,
+        }
+    }
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email);
@@ -23,6 +45,6 @@ export class AuthService {
             }
         }
         // se chegar aqui é pk nao encontrou um user/password
-        throw new Error('email address or password provided is incorrect')
+        throw new error('email address or password provided is incorrect')
     } 
 }
